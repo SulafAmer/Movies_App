44c8a92e13236d14595343cfbf4ac1cae129339c
SulafAmer <168731938+SulafAmer@users.noreply.github.com>
2026-08-13 16:26:33 +0300

    +Added static colors.

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static Color blackColor = Color(0xff121312);
+  static Color darkGrayColor = Color(0xff282A28);
+  static Color yellowColor = Color(0xffFFBB3B);
+  static Color whiteColor = Color(0xffFFFFFF);
+  static Color greenColor = Color(0xff57AA53);
+  static Color redColor = Color(0xffE82626);
+}
