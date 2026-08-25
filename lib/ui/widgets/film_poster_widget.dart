5ba0e1abe054b32fb4a4454536174d23e4f48b2b
Habi-ba <habibafadel88@gmail.com>
2026-08-25 04:49:53 +0300

    edited film poster widget to use cached network image for supporting states that come from the api

--- lib/ui/widgets/film_poster_widget.dart
+++ lib/ui/widgets/film_poster_widget.dart
@@ -1,50 +1,80 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:movies_app/utils/app_colors.dart';
 import 'package:movies_app/utils/app_styles.dart';
 import 'package:movies_app/utils/size_utils.dart';
 
 class FilmPosterWidget extends StatelessWidget {
-  double boxHeight;
-  double boxWidth;
-  double borderRadius;
-  String filmImage;
-  String filmRate;
-  double? horizontalMargin;
+  final double boxHeight;
+  final double boxWidth;
+  final double borderRadius;
+  final String filmImage;
+  final String filmRate;
+  final double? horizontalMargin;
 
-  FilmPosterWidget({
+  const FilmPosterWidget({
+    super.key,
     required this.boxHeight,
     required this.boxWidth,
     required this.borderRadius,
     required this.filmImage,
     required this.filmRate,
-    this.horizontalMargin
+    this.horizontalMargin,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: horizontalMargin ?? 0),
-      alignment: AlignmentDirectional.topStart,
+      margin: EdgeInsets.symmetric(
+        horizontal: horizontalMargin ?? 0,
+      ),
       height: context.scaleHeight(boxHeight),
       width: context.scaleWidth(boxWidth),
-      decoration: BoxDecoration(
+      child: ClipRRect(
         borderRadius: BorderRadius.circular(borderRadius),
-        image: DecorationImage(image: AssetImage(filmImage), fit: BoxFit.fill),
-      ),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: AppColors.transparentBlackColor,
-        ),
-        alignment: Alignment.center,
-        height: context.scaleHeight(30),
-        width: context.scaleWidth(58),
-        margin: EdgeInsets.all(10),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
+        child: Stack(
+          alignment: AlignmentDirectional.topStart,
           children: [
-            Text(filmRate, style: AppStyles.regular16White),
-            Icon(Icons.star, color: AppColors.yellowColor),
+
+            Positioned.fill(
+              child: CachedNetworkImage(
+                imageUrl: filmImage,
+                fit: BoxFit.cover,
+                placeholder: (context, url) => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+                errorWidget: (context, url, error) => const Center(
+                  child: Icon(Icons.error),
+                ),
+              ),
+            ),
+
+            // Rating
+            Container(
+              height: context.scaleHeight(30),
+              width: context.scaleWidth(58),
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: AppColors.transparentBlackColor,
+              ),
+              alignment: Alignment.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    filmRate,
+                    style: AppStyles.regular16White,
+                  ),
+                  const SizedBox(width: 3),
+                  Icon(
+                    Icons.star,
+                    color: AppColors.yellowColor,
+                    size: 18,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
