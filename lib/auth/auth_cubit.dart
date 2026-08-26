af5860dbc90090ebbf96bf824b77e19c1df782e9
Habi-ba <habibafadel88@gmail.com>
2026-08-26 04:58:49 +0300

    login wit email and password & Google using Firebase Authentication

---
+++ lib/auth/auth_cubit.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_app/auth/auth_states.dart';
+import 'package:movies_app/utils/firebase_utils.dart';
+
+class AuthCubit extends Cubit<AuthStates> {
+  AuthCubit() : super(AuthInitialState());
+  void login({required String email, required String password}) async {
+    emit(AuthLoadingState());
+    try {
+      await FirebaseUtils.login(email: email, password: password);
+      emit(AuthSuccessState());
+    } on FirebaseAuthException catch (e) {
+      emit(AuthErrorState(errorMessage: e.message.toString()));
+    } catch (e) {
+      emit(AuthErrorState(errorMessage: e.toString()));
+    }
+  }
+  void loginWithGoogle() async {
+    emit(AuthLoadingState());
+    try {
+      await FirebaseUtils.loginWithGoogle();
+      emit(AuthSuccessState());
+    } on FirebaseAuthException catch (e) {
+      emit(AuthErrorState(errorMessage: e.message.toString()));
+    } catch (e) {
+      emit(AuthErrorState(errorMessage: e.toString()));
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:movies_app/auth/auth_cubit.dart';
+import 'package:movies_app/firebase_options.dart';
 import 'package:movies_app/l10n/app_localizations.dart';
 import 'package:movies_app/providers/app_language_provider.dart';
 import 'package:movies_app/ui/login/forget_password_screen.dart';
@@ -7,11 +12,22 @@ import 'package:movies_app/ui/login/register_screen.dart';
 import 'package:movies_app/ui/screens/home_screen.dart';
 import 'package:movies_app/ui/screens/profile/update_profile_screen.dart';
 import 'package:movies_app/utils/app_routes.dart';
+import 'package:movies_app/utils/firebase_utils.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(ChangeNotifierProvider(create: (BuildContext context) { return AppLanguageProvider(); },
-  child: MoviesApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GoogleSignIn.instance.initialize();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    ChangeNotifierProvider(
+      create: (BuildContext context) {
+        return AppLanguageProvider();
+      },
+      child: MoviesApp(),
+    ),
+  );
 }
 
 class MoviesApp extends StatelessWidget {
@@ -19,20 +35,21 @@ class MoviesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var langProvider=Provider.of<AppLanguageProvider>(context);
+    var langProvider = Provider.of<AppLanguageProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: 'home_screen',
+      initialRoute: 'login_screen',
       routes: {
-        AppRoutes.loginScreenRouteName:(context) => LoginScreen(),
-        AppRoutes.registerScreenRouteName:(context) =>RegisterScreen() ,
+        AppRoutes.loginScreenRouteName: (context) => BlocProvider(
+          create: (BuildContext context) => AuthCubit(),
+          child: LoginScreen(),
+        ),
+        AppRoutes.registerScreenRouteName: (context) => RegisterScreen(),
         AppRoutes.forgetPasswordScreenRouteName: (context) =>
             ForgetPasswordScreen(),
         AppRoutes.updateProfileScreenRouteName: (context) =>
             UpdateProfileScreen(),
         AppRoutes.homeScreenRouteName: (context) => HomeScreen(),
-
-
       },
       locale: Locale(langProvider.appLanguage),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
